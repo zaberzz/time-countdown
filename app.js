eb677f5fb16ec07ccf240a164dd71af58298d021
Saba <saba.sadegh67@gmail.com>
2022-12-11 13:45:07 +0330

    set up future date dynamiclly

---
+++ app.js
@@ -0,0 +1,37 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector(".giveaway");
+const items = document.querySelectorAll(".deadline-format h4");
+const deadline = document.querySelector(".deadline");
+
+let futureDate = new Date(2023, 2, 23, 16, 30, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const date = futureDate.getDate();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
