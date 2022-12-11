836026d1070b08ca78c7c48e2f6fec2e95da4b04
Saba <saba.sadegh67@gmail.com>
2022-12-11 23:01:03 +0330

    caculating remaining time

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector(".giveaway");
 const items = document.querySelectorAll(".deadline-format h4");
 const deadline = document.querySelector(".deadline");
 
-let futureDate = new Date(2023, 2, 23, 16, 30, 0);
+let futureDate = new Date(2023, 0, 23, 16, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -35,3 +35,33 @@ const month = months[futureDate.getMonth()];
 const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
+
+const futureDateTime = futureDate.getTime();
+
+function remainingTime() {
+  const today = new Date().getTime();
+  const t = futureDateTime - today;
+
+  oneDay = 24 * 60 * 60 * 1000;
+  oneHour = 60 * 60 * 1000;
+  oneMin = 60 * 1000;
+
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMin);
+  let seconds = Math.floor((t % oneMin) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return `0${item}`;
+    } else {
+      return item;
+    }
+  }
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+}
+remainingTime();
